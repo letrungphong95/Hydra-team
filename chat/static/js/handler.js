abeb8efa3eafe078908d63f07a1ed64156a11b47
letrungphong95 <letrungphong95@gmail.com>
2019-06-24 22:55:51 +0700

    Up

--- chat/static/js/handler.js
+++ chat/static/js/handler.js
@@ -6,17 +6,19 @@ $(function(){
             type:'POST',
             url:'/test/',
             data:{
-                text:$('#input_msg').val(),
+                text:$('#m').val(),
                 csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val()
             },
             success:function(data){
-                tbody.append('<li class="me2" style="color:red">' +data['user_text']+ '</li>');
-                tbody.append('<li class="bot2" style="color:blue">' +data['bot_text']+ '</li>');
-                $('#messages').animate({
-                    scrollTop: $('#messages')[0].scrollHeight
-                }, 0);
-                $('#input_msg').val = '';
-                $('#input_msg').val('').focus();
+                if (data['user_text'].replace(/\s+/g, '') != ''){
+                    tbody.append('<li class="me" style="color:red">' +data['user_text']+ '</li>');
+                    tbody.append('<li class="bot" style="color:blue">' +data['bot_text']+ '</li>');
+                    $('#messages').animate({
+                        scrollTop: $('#messages')[0].scrollHeight
+                    }, 0);
+                }
+                $('#m').val = '';
+                $('#m').val('').focus();
             }
         }); 
     });
